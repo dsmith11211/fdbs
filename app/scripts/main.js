b301a0f73d65a0f05e2c3e4ab1a8ddb53a7d83f7
Derek Smith <dsmith@creativeworx.com>
2014-10-31 00:38:02 -0400

    Initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global FdbsQuiz, $*/
+
+
+window.FdbsQuiz = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    FdbsQuiz.init();
+});
